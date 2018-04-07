caa5d521f718d62a5168faf747b04542653733fb
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-07 20:19:59 +0200

    Comienzo de construcción de la aplicación
      * Añadidos nuevos elementos al controlador (app.component.ts)
        · Nombre, Curso y empresa
    
      * Añadida barra de navegación de Bootstrap a la plantilla del
      componente.
    
      * Añadidos estilos css de Bootstrap al layout principal y scripts
      necesarios.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  curso = 'Angular';
+  alumno = 'Juan Pedro Sánchez Álvarez';
+  empresa = 'Grupo Preving';
 }
