44bd87d470de370de8140d3f934d8b615dc53dd0
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-08 17:58:53 +0200

    Desacoplamiento del listado de clientes del componente.
      * Creación del archivo "clientes.json.ts" que ahora almacena la lista
      de clientes anteriormente implementada en el componente. El objetivo
      es simular el archivo JSON proveniente de un repositorio.

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,16 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Juan Pedro', apellido: 'Sánchez', email: 'juanpedro.sanchez@preving.com', createAt: '2017/12/11'},
+  {id: 2, nombre: 'María', apellido: 'de la Fuente', email: 'delafmaria@gmail.com', createAt: '2018/05/01'},
+  {id: 3, nombre: 'Leonardo', apellido: 'Benitez', email: 'leonardo.benitez@preving.com', createAt: '2016/03/21'},
+  {id: 4, nombre: 'Antonio', apellido: 'Melitón', email: 'antonio.meliton@preving.com', createAt: '2017/12/11'},
+  {id: 5, nombre: 'Jesús', apellido: 'Gonzalez', email: 'jesus.gonzalez@preving.com', createAt: '2018/05/01'},
+  {id: 6, nombre: 'Nieves', apellido: 'Rodríguez', email: 'nieves.rodriguez@preving.com', createAt: '2016/03/21'},
+  {id: 7, nombre: 'Víctor', apellido: 'Saavedra', email: 'vitosaavedra@gmail.com', createAt: '2017/12/11'},
+  {id: 8, nombre: 'Daniel', apellido: 'Curiel', email: 'curieldaniel@gmail.com', createAt: '2018/05/01'},
+  {id: 9, nombre: 'Fernando', apellido: 'Ceballos', email: 'fernandoceballos1989@gmail.com', createAt: '2016/03/21'},
+  {id: 10, nombre: 'Antonio', apellido: 'Guijarro', email: 'guijarroguijarro@gmail.com', createAt: '2017/12/11'},
+  {id: 11, nombre: 'Javier', apellido: 'Montesinos', email: 'javier.montesinos@preving.com', createAt: '2018/05/01'},
+  {id: 12, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2016/03/21'}
+];
