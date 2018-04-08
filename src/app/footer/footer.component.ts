d393260126959b6a5427ac8728a6d22776f705a2
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 01:24:17 +0200

    Instalación de librerías mediante Node:
      * Se han instalado las librerías desde Node.js mediante el comando
      npm que actualiza el archivo "package.json".
    
      * los archivos de librería requeridos han sido referenciados en el
      archivo ".angular.cli.json" dentro de los arrrays "styles" y "scripts"
      para ser reconocidos por Angular.
    
      * se han suprimido los directorios de librerías "css" y "js" que se
      encontraban dentro de la carpeta "assets".

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  public autor: any = {nombre: 'juan Pedro', apellido: 'Sánchez'};
+  public autor: any = {nombre: 'Juan Pedro', apellido: 'Sánchez'};
 }
