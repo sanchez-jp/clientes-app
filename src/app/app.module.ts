b96715e7002c9a6d5b1d3ba2b2b72d10ffabc7cc
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 15:21:45 +0200

    Creación del componente form y la vista del formulario:
      * Creado componente para formulario
      * Implementada vista de formulario
      * Importación de FormsModule en app.module para el uso de directiva
      ngModel y ngSubmit en la vista, directivas que realizan labores de
      mapeo al formulario.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import {ClientesComponent} from './clientes/clientes.component';
 import {ClienteService} from './clientes/cliente.service';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import {FormsModule} from '@angular/forms';
 
 /**
  * Array de rutas o mapeos a los componentes.
@@ -29,11 +31,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
