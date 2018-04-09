baf62bd1674a21f0072e6de4494d3bcbb4fa6b72
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 16:39:05 +0200

    Instalación de SweetAlert2:
      * Instalación de SweetAlert2 mediante npm para el envio de mensajes
      de alerta al usuario
    
      * Lanzamiento de mensaje de alerta tipo success desde el componente
      de formulario al crear un nuevo usuario.

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
 import {Router} from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -23,7 +24,10 @@ export class FormComponent implements OnInit {
    */
   public create(): void {
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      cliente => {
+        this.router.navigate(['/clientes']);
+        swal('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito`, 'success');
+      }
     );
   }
 
